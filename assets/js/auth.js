d137fa91d64a2c8ee497e1065cb9b278dfe64e0c
Nicer Dizon <n.dizon0313@student.tsu.edu.ph>
2022-10-27 04:21:25 +0800

    added login authentication

---
+++ assets/js/auth.js
@@ -0,0 +1,44 @@
+// ===== Initialize Variables ===== //
+const auth = firebase.auth();
+const database = firebase.database();
+
+// ===== Login Auth ===== //
+const loginForm = document.querySelector('#login-form');
+loginbtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    // Get user info
+    const email = loginForm['email'].value;
+    const password = loginForm['password'].value;
+
+    // Log the user in
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user)
+        loginForm.reset();
+        window.location.href = '/dashboard.html';
+        alert('Login successful');
+    })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        
+        alert('Login unsuccessful, invalid details. Error: ' + errorCode);
+    });
+});
+
+// ===== User Logout ===== //
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
+    alert('Admin logged out.');
+});
+
+// ===== Listen for Auth Status Changes ===== //
+auth.onAuthStateChange((user) => {
+    if (user) {
+        console.log('Admin logged in: ', user);
+    } else {
+        console.log('Admin logged out: ', user);
+    }
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,3 @@
-// ===== Login ===== //
-const inputs = document.querySelectorAll('.input-field');
-const toggle_btn = document.querySelectorAll('.toggle');
-const main = document.querySelector('.login-main');
-const bullets = document.querySelectorAll('.bullets .span');
-const images = document.querySelectorAll('.image');
-
-inputs.forEach((inp) => {
-    inp.addEventListener('focus', () => {
-        inp.classList.add('active');
-    });
-    inp.addEventListener('blur', () => {
-        if (inp.value != '') return;
-        inp.classList.remove('active');
-    });
-});
-
-
-function moveSlider() {
-    let index = this.dataset.value;
-
-    let currentImage = document.querySelector(`.img-${index}`);
-    images.forEach((img) => img.classList.remove('show'));
-    currentImage.classList.add('show');
-
-    const textSlider = document.querySelector('.text-group');
-    textSlider.style.transform = `translateY(${-(index - 1) * 2.2}rem)`;
-
-    bullets.forEach((bull) => bull.classList.remove('active'));
-    this.classList.add('active');
-}
-
-bullets.forEach((bullet) => {
-    bullet.addEventListener('click', moveSlider);
-});
-
-
 // ===== Sidebar Toggle ===== //
 const body = document.querySelector('body'),
     modeToggle = body.querySelector('.mode-toggle');
@@ -55,32 +18,24 @@ sidebarToggle.addEventListener('click', () => {
     }
 });
 
-
-// ===== Sidebar Active Listener  ===== // 
+// ===== Sidebar Active Listener  ===== //
 const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('nav a').forEach(link => {
-  if(link.href.includes(`${activePage}`)){
-    link.classList.add('active');
-    console.log(link);
-  }
+const navLinks = document.querySelectorAll('nav a').forEach((link) => {
+    if (link.href.includes(`${activePage}`)) {
+        link.classList.add('active');
+        console.log(link);
+    }
 });
 
-
-// ===== Dashboard Cards Listener  ===== // 
-document.getElementById("tenantsCard").onclick = function () {
-    location.href = "tenants.html";
+// ===== Dashboard Cards Listener  ===== //
+document.getElementById('tenantsCard').onclick = function () {
+    location.href = 'tenants.html';
 };
 
-document.getElementById("roomsCard").onclick = function () {
-    location.href = "rooms.html";
+document.getElementById('roomsCard').onclick = function () {
+    location.href = 'rooms.html';
 };
 
-document.getElementById("feedbacksCard").onclick = function () {
-    location.href = "feedbacks.html";
+document.getElementById('feedbacksCard').onclick = function () {
+    location.href = 'feedbacks.html';
 };
-
-
-/* ===== Include HTML Script ===== 
-$(function () {
-    $('#sidebar').load('sidebar.html');
-}); */
